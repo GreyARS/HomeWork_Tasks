2a80231785d73a538f6e814bbaa2406be645b586
ARS-Grey <axl_81@bk.ru>
2022-12-10 20:22:55 +0300

    Task 10 complete

--- Program.cs
+++ Program.cs
@@ -1,83 +1,16 @@
 ﻿/*
-// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Input a first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-if (num1 == num2)
+int MiddleNum(int num)
 {
-    Console.Write("This numbers is equal");
-    }
+    int mid = num % 100 /10;
 
-else 
-
-if (num1 > num2)
-{
-    Console.Write($"Number {num1} is bigger than number {num2}.");
+    return mid;
 }
 
-else
-{
-    Console.Write($"Number {num2} is bigger than number {num1}.");
-}
-*/
-
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
-
-/*
-Console.Write("Input the First number: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Input the Second number: ");
-int b = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Input the Third number: ");
-int c = Convert.ToInt32(Console.ReadLine());
-
-int max = a;
-
-if(b > max) max = b;
-if(c > max) max = c;
-
-Console.Write("Maximum number is: " + max);
-*/
-
-
-/*
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
-Console.Write("Input number: ");
+Console.WriteLine("Введите трёхзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if(num % 2 == 0)
-{
-    Console.Write($"The number {num} is even.");
-}
-else
-{
-    Console.Write($"The number {num} is NOT even.");
-}
-*/
-
-// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
-
-/*
-Console.Write("Input number: ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-int current = 1;
-
-while(current < N)
-{
-    current += 1;
-    Console.Write(current + " ");
-    current++;
-}
-
-Console.WriteLine();
+int middle = MiddleNum(num);
+Console.Write("Вторая цифра трёхзначного числа - " + middle);
 */

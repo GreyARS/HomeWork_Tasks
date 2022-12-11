2f762b3c348aa3bbffc7eebd91fba8cbea36f41f
ARS-Grey <axl_81@bk.ru>
2022-12-11 22:02:22 +0300

    Task 21 complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿/*
+// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 bool IsPal(int num)
 {
@@ -27,3 +28,29 @@ Console.Write("Input a number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 bool Palyndrom = IsPal(num);
+*/
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double Range(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    double lenght = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
+    return lenght;
+}
+
+Console.Write("Введите координату точки А по оси X: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки А по оси Y: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки А по оси Z: ");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки B по оси X: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки B по оси Y: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки B по оси Z: ");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double rangeAB = Range(xA, yA, zA, xB, yB, zB);
+Console.Write("Range of AB = " + rangeAB);

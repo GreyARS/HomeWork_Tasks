277899777daa2445ebbe433ea9c2ebd86b296475
ARS-Grey <axl_81@bk.ru>
2022-12-14 22:48:40 +0300

    Task 27 complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿/*
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 int ToDegree(int a, int b)
 
@@ -31,3 +32,26 @@ int b = Convert.ToInt32(Console.ReadLine());
 
 int result = ToDegree(a, b);
 Console.Write($"{a} to the {b} degree is " + result);
+*/
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int NumberSum(int num)
+{
+    int sum = 0;
+    for(int element = 0; num > 0; element++)
+    {
+        element = num % 10;
+        num = num / 10;
+        sum += element;
+    }
+    return sum;
+}
+
+Console.Write("Input a Number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = NumberSum(num);
+Console.Write($"The Sum of the elements of Number {num} is {result}");

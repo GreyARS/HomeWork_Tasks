c683a2fa569b34120e7cd8a45845bbea367056eb
ARS-Grey <axl_81@bk.ru>
2022-12-16 20:11:26 +0300

    Task 38 complete

--- Program.cs
+++ Program.cs
@@ -44,6 +44,7 @@ Console.Write("The number of even positive elements in the array is " + result);
 */
 
 
+/*
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 int[] CreateRandomArray(int size, int min, int max)
@@ -87,3 +88,69 @@ ShowArray(myArray);
 
 int sum = SumOddPositions(myArray);
 Console.Write("Sum of elements in odd position is " + sum);
+*/
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateDoubleArray(int size, int min, int max)
+{
+    double[] array = new double[size];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().Next(min, max) + new Random().NextDouble(), 2);
+    }
+    return array;
+}
+
+void ShowArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("[" + array[i] + "] ");
+    }
+    Console.WriteLine();
+}
+
+double MaxValue(double[] array)
+{
+    double maxValue = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > maxValue) maxValue = array[i];
+    }
+    return maxValue;
+}
+
+double Difference(double[] array)
+{
+    double minValue = array[0];
+    double maxValue = array[0];
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < minValue) minValue = array[i];
+        if(array[i] > maxValue) maxValue = array[i];
+    }
+
+    Console.WriteLine($"Maximum value is [{maxValue}]");
+    Console.WriteLine($"Minimum value is [{minValue}]");
+
+    double difference = maxValue - minValue;
+    return difference;
+}
+
+Console.Write("Input Number of Elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible Value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible Value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] myArray = CreateDoubleArray(size, min, max);
+ShowArray(myArray);
+
+double maxValue = MaxValue(myArray);
+
+double diff = Difference(myArray);
+Console.WriteLine($"The Difference between minValue and maxValue is [{diff}]");

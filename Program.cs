bcff3447c5875abcc7f76369a75639af4a5cebc9
ARS-Grey <axl_81@bk.ru>
2023-01-08 15:52:11 +0300

    Task 56 complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿/*
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
 void SortArray(int[,] array)
 {
@@ -52,3 +53,67 @@ WriteArray(array);
 Console.WriteLine("Отсортированный массив: ");
 SortArray(array);
 WriteArray(array);
+*/
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for(int j = 1; j < array.GetLength(1); j++)
+    sumLine += array[i,j];  
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array, int min, int max)
+{
+  for(int i = 0; i < array.GetLength(0); i++)
+    for(int j = 0; j < array.GetLength(1); j++)
+      array[i, j] = new Random().Next(min, max);
+}
+
+void ShowArray(int[,] array)
+{
+  Console.WriteLine();
+  for(int i = 0; i < array.GetLength(0); i++)
+  {
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + "\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.WriteLine("Введите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int min = InputNumbers("Введите минимальное значение: ");
+int max = InputNumbers("Введите максимальное значение: ");
+
+int[,] array = new int[m, n];
+CreateArray(array, min, max);
+ShowArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+
+for(int i = 1; i < array.GetLength(0); i++)
+{
+  int temp = SumLineElements(array, i);
+  if(sumLine > temp)
+  {
+    sumLine = temp;
+    minSumLine = i;
+  }
+}
+Console.WriteLine();
+Console.WriteLine($"{minSumLine + 1} - строкa с наименьшей суммой элементов({sumLine}).");

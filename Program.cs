15d4716fd2dca71cc729b3553dae19c5df6d3356
ARS-Grey <axl_81@bk.ru>
2023-01-09 11:21:25 +0300

    Task 66 complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
 void ShowNums(int num)
@@ -11,3 +11,32 @@ Console.Write("Input number N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 ShowNums(n);
+*/
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+void SumNumbers(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumNumbers(m, n);
